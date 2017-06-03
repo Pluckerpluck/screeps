1b317a814b700e02b9e36578b248491f98a91339
Samuel Gillespie <sammygillespie@gmail.com>
2017-06-03 14:40:07 +0100

    Initial project commit with basic AI
    Creation of a full build environment using gradle, typescript and
    webpack in order to both build and deploy a typscript project into a
    format suitible for Screeps.

---
+++ src/main/ts/role.harvester.ts
@@ -0,0 +1,24 @@
+export class RoleHarvester {
+
+    static run(creep:Creep) {
+        if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES) as Source[];
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.energy < structure.energyCapacity;
+                }
+            }) as Structure[];
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+        }
+    }
+}
